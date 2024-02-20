7b96f5f876c81b8c2842870d286fa030ed3174c4
rubenkretek <rubenkretek@outlook.com>
2024-02-20 12:20:23 +0000

    Refactored Routes

--- public/index.php
+++ public/index.php
@@ -1,6 +1,14 @@
 <?php
 require '../helpers.php';
 
-$uri = $_SERVER['REQUEST_URI'];
 
-require basePath('router.php');
+require basePath('Router.php');
+
+$router = new Router();
+
+$routes = require basePath('routes.php');
+
+$uri = $_SERVER['REQUEST_URI'];
+$method = $_SERVER['REQUEST_METHOD'];
+
+$router->route($uri, $method);
